6b08022e3b455444df2f10156a9d25233625b587
Radhika <radhikaarada4@gmail.com>
2024-09-17 15:37:30 +0530

    changes

---
+++ ec2/ec2.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "ssh-all-terraform"{
+
+description = "allow ssh"
+name = "ssh"
+
+#egress is same as outbound rule in AWS, which allows outgoing traffic
+egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+}
