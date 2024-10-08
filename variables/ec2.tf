b54853e76612f87e32a9be05d21a8a9778cbfc64
Radhika <radhikaarada4@gmail.com>
2024-10-08 15:03:40 +0530

    new changes:

---
+++ variables/ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "Secure_SSH" {
+  name        = var.sg_name
+  description = "allow secure connections"
+
+  egress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = var.protocol
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = var.cidr_ingress
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = var.tags
+}
+
+resource "aws_instance" "Secure_demo_instance" {
+    
+  ami                     = var.ami_id
+  instance_type           = var.instance_type
+  vpc_security_group_ids = [aws_security_group.Secure_SSH.id]
+
+  tags = var.tags
+
+
+  }
